1828fd7c789c7e5dfdb675b78e24be323e65bb6a
polatengin <polatengin@outlook.com>
2020-05-05 18:05:42 +0300

    answer button variable created

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,6 +3,7 @@ const API_HOST: string = 'localhost';
 const API_PORT: number = 3000;
 const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || (navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 const registerButton = document.getElementById('register-button') as HTMLButtonElement;
+const answerButton = document.getElementById('answer-button') as HTMLButtonElement;
 let searchText: string = '';
 
 if (getUserMedia === null) {
