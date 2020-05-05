709859712e15b3ce88a568e3e40e17fab95884f3
polatengin <polatengin@outlook.com>
2020-05-05 16:43:40 +0300

    default route created

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,9 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.status(200).send('Baku is working 🥳');
+});
 const server = http.createServer(app);
 
 server.listen(port, () => {
