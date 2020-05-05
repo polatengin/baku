d8e531f676a1a1436750b102553154e2d95e0adb
polatengin <polatengin@outlook.com>
2020-05-05 18:05:27 +0300

    global variables to server api

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,6 @@
+const API_SECURE: boolean = false;
+const API_HOST: string = 'localhost';
+const API_PORT: number = 3000;
 const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || (navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 const registerButton = document.getElementById('register-button') as HTMLButtonElement;
 let searchText: string = '';
