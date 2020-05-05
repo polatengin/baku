8cfdd706760086657e0645d167fc146857a8a3bb
polatengin <polatengin@outlook.com>
2020-05-05 16:45:10 +0300

    cors support added to server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,10 @@
 const http = require('http');
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors({origin: 'http://localhost:5000'}));
 app.get('/', (req, res) => {
   res.status(200).send('Baku is working 🥳');
 });
