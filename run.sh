244a59ab5d9c8e6b7e8101ba01523a2d218ef5b7
polatengin <polatengin@outlook.com>
2020-05-05 16:21:59 +0300

    if client and server project ports are in use, kill the apps/ports then continue running projects

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
+PORTS=(3000 5000)
+
+for PORT in ${PORTS[@]}; do
+  PID=$( lsof -i :$PORT | awk '{ print $2 }' | sed 's/PID//g' | sed -n '2 p' )
+
+  if [ -n "$PID" ]; then
+    kill "$PID"
+  fi
+done
+
 cd ./server
 npm run start &
 
