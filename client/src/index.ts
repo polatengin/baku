ed9de9994f04436092edbce40d640093940978de
polatengin <polatengin@outlook.com>
2020-05-05 18:09:02 +0300

    global variables to store video elements

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,10 +1,16 @@
 import Peer from 'peerjs';
+
 const API_SECURE: boolean = false;
 const API_HOST: string = 'localhost';
 const API_PORT: number = 3000;
+
 const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || (navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+
 const registerButton = document.getElementById('register-button') as HTMLButtonElement;
 const answerButton = document.getElementById('answer-button') as HTMLButtonElement;
+const videoMe = document.getElementById('video-me') as HTMLVideoElement;
+const videoOther = document.getElementById('video-other') as HTMLVideoElement;
+
 let searchText: string = '';
 
 if (getUserMedia === null) {
