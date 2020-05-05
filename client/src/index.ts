98ba40f13c7fdab6101143cdda26c1c1eb941794
polatengin <polatengin@outlook.com>
2020-05-05 18:04:36 +0300

    check if page has access to users cam and mic, if not, show fail alert

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,2 +1,8 @@
 const registerButton = document.getElementById('register-button') as HTMLButtonElement;
 let searchText: string = '';
+
+if (getUserMedia === null) {
+  const mediaAlertFail = document.getElementById('media-alert-fail') as HTMLDivElement;
+
+  mediaAlertFail.classList.remove('d-none');
+}
