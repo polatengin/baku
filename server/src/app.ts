2d78eaa1fdf63e89ecc3396f4c27c6af1f5b11d2
polatengin <polatengin@outlook.com>
2020-05-05 16:45:39 +0300

    body parser added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,4 @@
+import * as bodyParser from 'body-parser';
 const http = require('http');
 const express = require('express');
 const cors = require('cors');
@@ -5,6 +6,9 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({origin: 'http://localhost:5000'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.status(200).send('Baku is working 🥳');
 });
