20934e1714ad240aaf4d83fe458b3a4f14682fde
polatengin <polatengin@outlook.com>
2020-05-05 19:10:27 +0300

    port changed with 80

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,9 +8,9 @@ const cors = require('cors');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = 80;
 
-app.use(cors({origin: ['http://localhost:5000', 'https://polatengin-baku-client.netlify.app']}));
+app.use(cors({ origin: 'https://polatengin-baku-client.netlify.app' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
